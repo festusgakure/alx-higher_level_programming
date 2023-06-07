c5379863cfabdae9fa498c0313124852595ba530
festusgakure <festusgakure@gmail.com>
2023-06-07 06:33:31 -0700

    Made code pycode compliant

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-number = int(number)
 mod = abs(number) % 10
 if number < 0:
     mod = -mod
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for numb in range(97,123):
-    if chr(numb) not in ['e','q']:
+for numb in range(97, 123):
+    if chr(numb) not in ['e', 'q']:
         print("{}".format(chr(numb)), end="")
