4fb5355228f69118e0b1851ef37553aa6dd0910a
festusgakure <festusgakure@gmail.com>
2023-06-12 13:07:56 -0700

    Changed output to a decimal value

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        print(" ".join("{}".format(n) for n in row))
+        print(" ".join("{:d}".format(n) for n in row))
