afcaf71f011d8c8c50217e85a2bc7a95a5348335
festusgakure <festusgakure@gmail.com>
2023-06-14 12:26:52 -0700

    Made code pycode compliant

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    #Create an empty array of the same size as matrix
+    # Create an empty array of the same size as matrix
     rows = len(matrix)
     col = len(matrix[0])
     new_matrix = [[0 for _ in range(col)] for _ in range(rows)]
 
     for i in range(rows):
         for j in range(len(matrix[i])):
-            new_matrix[i][j]=matrix[i][j]**2
+            new_matrix[i][j] = matrix[i][j]**2
     return new_matrix
