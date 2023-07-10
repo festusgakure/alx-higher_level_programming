404f88f6f6f24f92356b57fc3b1a9841e5ddf627
festusgakure <festusgakure@gmail.com>
2023-07-10 13:30:36 -0700

    Same class or inherit from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Contains one module
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Checks instance if True or False
+    """
+    return isinstance(obj, a_class)
