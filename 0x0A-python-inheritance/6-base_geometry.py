ee3ea8bf2a4da0c8de9ee9c0d5b897fdedc04169
festusgakure <festusgakure@gmail.com>
2023-07-10 13:45:57 -0700

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Contain one module
+"""
+
+
+class BaseGeometry:
+    """
+    Empty Class
+    """
+    def area(self):
+        """
+        Class BaseGeometry 
+        """
+        raise Exception("area() is not implemented")
